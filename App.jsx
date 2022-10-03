79acb8a21447678544a62682f2def211c4c971f8
sumitmahi <113238807+sumitmahi@users.noreply.github.com>
2022-10-03 23:15:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Address from "./Component/Address";
+import Home from "./Component/Home";
+import Navbar from "./Component/Navbar";
+
+const App = () => {
+  return (  
+    <div>
+    <Navbar/>
+    <Home/>
+    <Address/>
+    </div>
+  );
+}
+ 
+export default App;
+
